bf7a5b80013cb526299c866e959ab50474fec099
TomRobson <tomrobson19@gmail.com>
2016-10-27 12:55:28 +0100

    ransac implementation

--- ComputerVision/computerVision.py
+++ ComputerVision/computerVision.py
@@ -19,6 +19,7 @@
 import cv2
 import os
 import numpy as np
+import random
 
 directory_to_cycle = "road-images2016-DURHAM"
 
@@ -37,25 +38,56 @@ for filename in os.listdir(directory_to_cycle):
 
         img = cv2.imread(os.path.join(directory_to_cycle, filename), cv2.IMREAD_COLOR)
 
+        #remove the top 60% of the image to focus on the roads
         height = img.shape[0]
-        img = img[(int)(height/2):height,0:img.shape[1]]
+        img = img[(int)(6*height/10):height,0:img.shape[1]]
 
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #this or hsv?
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-        canny = cv2.Canny(gray, 100, 200)
+        #can convert to grayscale using weighted sum of just h and s
+
+
+
+
+
+
+        smoothed = cv2.GaussianBlur(hsv,(5,5),0)
+
+        canny = cv2.Canny(hsv, 100, 200)
 
         data = cv2.findNonZero(canny)
 
+        t = 2
+        v = 300
+        trials = 10000 #try only counting as a trial if long enough
+        points = []
+        #random.seed(53)#seed random generator for testing
+        for i in range(trials):
+            
+            r1 = random.randint(0,len(data)-1)
+            r2 = random.randint(0,len(data)-1)
+
+            points = [data[r1][0],data[r2][0]] #select points from non zeros
+
+            lines = np.zeros((canny.shape[0],canny.shape[1]), np.uint8)
+            lines = cv2.line(lines,(points[0][0],points[0][1]),(points[1][0],points[1][1]),255,2)
 
 
+            compare =  np.zeros((canny.shape[0],canny.shape[1]), np.uint8)
+            compare = cv2.bitwise_and(canny,lines)
 
+            pixels = cv2.findNonZero(compare)
 
+            if (len(pixels) > v):
+                img = cv2.line(img,(points[0][0],points[0][1]),(points[1][0],points[1][1]),(0,0,255))
+
+            #gradient = (points[0][0]-points[1][0])/(points[0][1]-points[1][1])
 
 
 
 
         canny = cv2.cvtColor(canny,cv2.COLOR_GRAY2BGR)
-        out = np.concatenate((img,canny),axis=1)
+        out = np.concatenate((img,canny),axis=0)
 
         cv2.imshow('image',out)
         key = cv2.waitKey(1000000000) # wait for this long or until any key press
