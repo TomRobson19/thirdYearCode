e68be01a1c92819fc694512ee36e05d037e1cf3f
Tom <tomrobson19@gmail.com>
2016-10-31 14:46:44 +0000

    "Implemented wrap around forces"

--- NumericalAlgorithms/numericalAlgorithms.c
+++ NumericalAlgorithms/numericalAlgorithms.c
@@ -14,10 +14,10 @@
 #include <math.h>
 
 const int N = 2; //number of particles
-const double timeStepSize = 5000000;
-const int timeSteps = 2000;
-const int plotEveryKthStep = 1000;
-const double a = pow(10,-5); //constant value of a and s
+const double timeStepSize = pow(10,-4);
+const int timeSteps = 2000000;
+const int plotEveryKthStep = 100;
+const double a = 0.1; //constant value of a and s
 
 double x[N][3];
 double v[N][3];
@@ -26,12 +26,12 @@ void setUp(int N) //support arbitrary number of particles
 { 
 
   x[0][0] = 0.4;
-  x[0][1] = 0.0;
-  x[0][2] = 0.0;
+  x[0][1] = 0.5;
+  x[0][2] = 0.5;
 
   x[1][0] = 0.6;
-  x[1][1] = 0.0;
-  x[1][2] = 0.0;
+  x[1][1] = 0.5;
+  x[1][2] = 0.5;
 
   for (int i=0; i<N; i++)
   {
@@ -81,65 +81,95 @@ void updateBody(int N)
         double yDist = x[i][1]-x[j][1];
         double zDist = x[i][2]-x[j][2];
 
-        const double distance = sqrt(xDist * xDist + yDist * yDist + zDist * zDist);
+        double distance = sqrt((xDist*xDist) + (yDist*yDist) + (zDist*zDist));
 
-        double f = (4*a*((12*pow(a,12))/pow(distance, 13) - (6*pow(a,6))/pow(distance, 7)));
+        double f = (4*a*(((12*pow(a,12))/pow(distance, 13)) - ((6*pow(a,6))/pow(distance, 7))));
        
-        force[0] += (xDist)/distance * f;
-        force[1] += (yDist)/distance * f;
-        force[2] += (zDist)/distance * f;
+        force[0] += xDist/distance * f;
+        force[1] += yDist/distance * f;
+        force[2] += zDist/distance * f;
         
         double newX;
         double newY;
         double newZ;
 
-        if (xDist < 0)
+        if(xDist > 0)
         {
-          newX = -1 + xDist;
+          newX = -1+xDist;
         }
-        else if (xDist > 0)
+        else if(xDist < 0)
         {
-          newX = 1 + xDist;
+          newX = 1+xDist;
         }
-        if (yDist < 0)
+        else
         {
-          newY = -1 + yDist;
+          newX = 0.0;
         }
-        else if (yDist > 0)
+        if(yDist > 0)
         {
-          newY = 1 + yDist;
+          newY = -1+yDist;
         }
-        if (zDist < 0)
+        else if(yDist < 0)
         {
-          newZ = -1 + zDist;
+          newY = 1+yDist;
         }
-        else if (zDist > 0)
+        else
         {
-          newZ = 1 + zDist;
+          newY = 0.0;
+        }
+        if(zDist > 0)
+        {
+          newZ = -1+zDist;
+        }
+        else if(zDist < 0)
+        {
+          newZ = 1+zDist;
+        }
+        else
+        {
+          newZ = 0.0;
         }
         
-        const double newDistance = sqrt(newX * newX + newY * newY + newZ * newZ);
+        double newDistance = sqrt((newX*newX) + (newY*newY) + (newZ*newZ));
         
-        double newF = (4*a*((12*pow(a,12))/pow(newDistance, 13) - (6*pow(a,6))/pow(newDistance, 7)));
+        double newF = (4*a*(((12*pow(a,12))/pow(newDistance, 13)) - ((6*pow(a,6))/pow(newDistance, 7))));
 
-        force[0] += (newX)/newDistance * newF;
-        force[1] += (newY)/newDistance * newF;
-        force[2] += (newZ)/newDistance * newF;
-
-        v[i][0] += timeStepSize * force[0];
-        v[i][1] += timeStepSize * force[1];
-        v[i][2] += timeStepSize * force[2];
+        force[0] += newX/newDistance * newF;
+        force[1] += newY/newDistance * newF;
+        force[2] += newZ/newDistance * newF;
       } 
     }
+    v[i][0] += timeStepSize * force[0];
+    v[i][1] += timeStepSize * force[1];
+    v[i][2] += timeStepSize * force[2];
 
-    for (int l=0; l<3; l++)
+    x[i][0] += timeStepSize * v[i][0];
+    x[i][1] += timeStepSize * v[i][1];
+    x[i][2] += timeStepSize * v[i][2];
+    
+    if(x[i][0]<=0)
     {
-      x[i][l] += timeStepSize * v[i][l];
-
-      if (x[i][l] < 0.0 || x[i][l] > 1.0)
-      {
-        x[i][l] -= floor(x[i][l])/1;
-      }
+      x[i][0] = 1;
+    }
+    if(x[i][0]>1)
+    {
+      x[i][0] = 0;
+    }
+    if(x[i][1]<=0)
+    {
+      x[i][1] = 1;
+    }
+    if(x[i][1]>1)
+    {
+      x[i][1] = 0;
+    }
+    if(x[i][2]<=0)
+    {
+      x[i][2] = 1;
+    }
+    if(x[i][2]>1)
+    {
+      x[i][2] = 0;
     }
   }
 }
