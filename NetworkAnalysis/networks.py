cc200cbb85c1916f5335b4ec1472ceb91ad85f68
unknown <unknown@unknown.com>
2017-02-11 21:56:06 +0000

    Implemented step 1 logic

--- NetworkAnalysis/networks.py
+++ NetworkAnalysis/networks.py
@@ -2,3 +2,31 @@
 Code for the Networks Assignment
 Tom Robson - hzwr87
 """
+import random
+
+
+def makeGroupGraph(m,k,p,q):
+	random.seed(m*k)
+	numberOfNodes = m*k
+	graph = {}
+	group = {}
+
+	#store groups of the nodes
+	for node in range(numberOfNodes):
+		group[node] = node//k
+		graph[node] = []
+
+	print(group)
+
+	for node in range(numberOfNodes):
+		for neighbour in range(node+1, numberOfNodes):
+			if(group[node] == group[neighbour] and random.random() < p):
+				graph[node] += [neighbour]
+				graph[neighbour] += [node]
+			elif(group[node] != group[neighbour] and random.random() < q):
+				graph[node] += [neighbour]
+				graph[neighbour] += [node]
+
+	print(graph)
+
+makeGroupGraph(5,4,0.5,0.2)
