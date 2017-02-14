f977910127afa0e30f588472d14db63c86c5e850
unknown <unknown@unknown.com>
2017-02-14 14:57:08 +0000

    Copied accross code for plotting

--- NetworkAnalysis/networksPart1.py
+++ NetworkAnalysis/networksPart1.py
@@ -83,6 +83,55 @@ def plotDegreeDistribution(graph):
 
 #############################################################################################################################
 
+def plot_diameter_vs_p(num_groups, nodes_per_group, external_prob, trials=1, title=False):
+    """plot diameter of group graph versus internal probability by taking average of k trials for each data point"""
+    #create arrays for plotting
+    xdata = []
+    ydata = []
+    for internal_prob in [0.1*p for p in range(1,11)]:
+        diameters = []
+        print (internal_prob)
+        for idx in range(trials):
+            graph = make_group_graph(num_groups, nodes_per_group, internal_prob, external_prob)
+            diam = diameter(graph)
+            diameters += [diam]
+        xdata += [internal_prob]
+        ydata += [1.0*sum(diameters)/trials]
+    plt.clf() #clears plot
+    plt.xlabel('Internal Probability')
+    plt.ylabel('Diameter')
+    if title:
+        plt.title(title)
+    plt.plot(xdata, ydata, marker='.', linestyle='-', color='b')
+    plt.savefig('Q1_diameters.png')
+
+#############################################################################################################################
+
+def question1(part1=True, part2=True):
+    if part1:
+        print ("construct group graphs with 400 vertices, divided into 20 groups of 20")
+        print ("let p take the values 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, and q=0.5 - p")
+        print ("for each value create 100 graphs, find on average how many vertices have degree d, and plot the distribution")
+        for p in [0.25, 0.3, 0.35, 0.4, 0.45, 0.5]:
+            print ("making the plot for p="+str(p))
+            q = 0.5 - p
+            dist = average_degree_distribution_group_graphs(20, 20, p, q, 100)
+            plot_degree_distribution(dist, "Degree Distribution of Group Graph with m=k=20, p="+str(p),"Q1_group_graph_p="+str(p)+"_degrees.png")
+        print ("repeat with graphs on 400 vertices, divided into 200 groups of 2 with p=0.4")
+        dist = average_degree_distribution_group_graphs(200, 2, 0.4, 0.1, 100)
+        plot_degree_distribution(dist, "Degree Distribution of Group Graph with m=200, k=2, p=0.4","Q1_group_graph_m=200_degrees.png")
+        print ("repeat with graphs on 400 vertices, divided into 2 groups of 200 with p=0.4")
+        dist = average_degree_distribution_group_graphs(2, 200, 0.4, 0.1, 100)
+        plot_degree_distribution(dist, "Degree Distribution of Group Graph with m=2, k=200, p=0.4","Q1_group_graph_m=2_degrees.png")
+        print ("looking at the figures produced, notice that the shapes of the distributions are all very similar and the degrees are all very close to the average")
+        print ()
+    if part2:
+        print ("to investigate the relationship between the diameter and p, construct, for each value of p, 20 group graphs with 80 groups, each on 5 vertices with q=0.05")
+        print ("doing the calculations for p=")
+        plot_diameter_vs_p(80, 5, 0.05, 20, "Diameter vs p for Group Graphs containing 80 groups of 5 vertices")
+        print ("from the plot we see the relationship is rather uninteresting with the diameter not depending (much) on p")
+        print ()
+
 print("Graph")
 
 print("Diameter")
