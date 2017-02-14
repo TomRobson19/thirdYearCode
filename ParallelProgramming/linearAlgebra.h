44c152cc493a2211f1cb6763ec3b92fb236cbdd4
unknown <unknown@unknown.com>
2017-02-14 12:38:46 +0000

    Parallel programming files

---
+++ ParallelProgramming/linearAlgebra.h
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+double* vector_vector_addition(double* a, double *b, int length);
+
+double vector_vector_dot_prod(double *a, double *b, int length);
+
+double* matrix_vector_multiplication(double **mat, double *vec, int rows, int cols);
+
+double** matrix_matrix_multiplication(double **a, double **b, int rows_a, int columns_a, int rows_b);
