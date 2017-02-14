2be2a40df33e15e8c7769ce10a1d2b75fa6c0a0b
unknown <unknown@unknown.com>
2017-02-14 13:24:28 +0000

    Copied over graph code for step 3

--- NetworkAnalysis/networksPart3.py
+++ NetworkAnalysis/networksPart3.py
@@ -7,3 +7,122 @@ import queue
 import numpy as np
 import matplotlib.pyplot as plt
 
+#############################################################################################################################
+
+def makeGroupGraph(m,k,p,q):
+	random.seed(m*k)
+	numberOfNodes = m*k
+	graph = {}
+	group = {}
+
+	#store groups of the nodes
+	for node in range(numberOfNodes):
+		group[node] = node//k
+		graph[node] = []
+
+	for node in range(numberOfNodes):
+		for neighbour in range(node+1, numberOfNodes):
+			if(group[node] == group[neighbour] and random.random() < p):
+				graph[node] += [neighbour]
+				graph[neighbour] += [node]
+			elif(group[node] != group[neighbour] and random.random() < q):
+				graph[node] += [neighbour]
+				graph[neighbour] += [node]
+
+	return graph
+
+#############################################################################################################################
+
+def make_random_graph(num_nodes, prob):
+    """Returns a dictionary to a random graph with the specified number of nodes
+    and edge probability.  The nodes of the graph are numbered 0 to
+    num_nodes - 1.  For every pair of nodes, i and j, the pair is considered
+    twice: once to add an edge (i,j) with probability prob, and then to add an
+    edge (j,i) with probability prob. 
+    """
+    #initialize empty graph
+    random_graph = {}
+    #consider each vertex
+    for vertex in range(num_nodes):
+        out_neighbours = []
+        for neighbour in range(num_nodes):
+            if vertex != neighbour:
+                random_number = random.random()
+                if random_number < prob:
+                    out_neighbours += [neighbour]        
+        #add vertex with list of out_ neighbours
+        random_graph[vertex] = set(out_neighbours)
+    return random_graph
+
+#############################################################################################################################
+
+class PATrial:
+    """
+    Used when each new node is added in creation of a PA graph.
+    Maintains a list of node numbers with multiple instances of each number.
+    The number of instances of each node number are in proportion to the
+    probability that it is linked to.
+    Uses random.choice() to select a node number from this list for each trial.
+    """
+
+    def __init__(self, num_nodes):
+        """
+        Initialize a PATrial object corresponding to a 
+        complete graph with num_nodes nodes
+        
+        Note the initial list of node numbers has num_nodes copies of
+        each node number
+        """
+        self._num_nodes = num_nodes
+        self._node_numbers = [node for node in range(num_nodes) for dummy_idx in range(num_nodes)]
+
+
+    def run_trial(self, num_nodes):
+        """
+        Conduct num_node trials using by applying random.choice()
+        to the list of node numbers
+        
+        Updates the list of node numbers so that the number of instances of
+        each node number is in the same ratio as the desired probabilities
+        
+        Returns:
+        Set of nodes
+        """       
+        #compute the neighbors for the newly-created node
+        new_node_neighbors = set()
+        for dummy_idx in range(num_nodes):
+            new_node_neighbors.add(random.choice(self._node_numbers))
+        # update the list of node numbers so that each node number 
+        # appears in the correct ratio
+        self._node_numbers.append(self._num_nodes)
+        self._node_numbers.extend(list(new_node_neighbors))        
+        #update the number of nodes
+        self._num_nodes += 1
+        return new_node_neighbors
+    
+def make_complete_graph(num_nodes):
+    """Takes the number of nodes num_nodes and returns a dictionary
+    corresponding to a complete directed graph with the specified number of
+    nodes. A complete graph contains all possible edges subject to the
+    restriction that self-loops are not allowed. The nodes of the graph should
+    be numbered 0 to num_nodes - 1 when num_nodes is positive. Otherwise, the
+    function returns a dictionary corresponding to the empty graph."""
+    #initialize empty graph
+    complete_graph = {}
+    #consider each vertex
+    for vertex in range(num_nodes):
+        #add vertex with list of neighbours
+        complete_graph[vertex] = set([j for j in range(num_nodes) if j != vertex])
+    return complete_graph
+    
+def make_PA_Graph(total_nodes, out_degree):
+    """creates a PA_Graph on total_nodes where each vertex is iteratively
+    connected to a number of existing nodes equal to out_degree"""
+    #initialize graph by creating complete graph and trial object
+    PA_graph = make_complete_graph(out_degree)
+    trial = PATrial(out_degree)
+    for vertex in range(out_degree, total_nodes):
+        PA_graph[vertex] = trial.run_trial(out_degree)
+    return PA_graph
+
+#############################################################################################################################
