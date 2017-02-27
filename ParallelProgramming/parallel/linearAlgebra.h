2849dead6d0acd7fbb0853122f110dc768c33753
unknown <unknown@unknown.com>
2017-02-27 11:16:57 +0000

    Reformatted parallel folders

--- ParallelProgramming/parallel/linearAlgebra.h
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-
-double* vector_vector_addition(double* a, double *b, int length);
-
-double vector_vector_dot_prod(double *a, double *b, int length);
-
-double* matrix_vector_multiplication(double **mat, double *vec, int rows, int cols);
-
-double** matrix_matrix_multiplication(double **a, double **b, int rows_a, int columns_a, int rows_b);
