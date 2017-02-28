d4150e70c106bb2ba13a4acaf750c430d42dee13
unknown <unknown@unknown.com>
2017-02-28 15:02:53 +0000

    Set up files

---
+++ ParallelProgramming/parallel/matMat.c
@@ -0,0 +1,5 @@
+//mpicc -g3 matMat.c -o matMat
+//mpirun -n 4 ./matMat
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
---
+++ ParallelProgramming/parallel/matVec.c
@@ -0,0 +1,5 @@
+//mpicc -g3 matVec.c -o matVec
+//mpirun -n 4 ./matVec
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
