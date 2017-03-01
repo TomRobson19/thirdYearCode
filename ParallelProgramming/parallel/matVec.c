2bd9c0b96dbf412610c8a69f9aae4e9b9dda4bdc
unknown <unknown@unknown.com>
2017-03-01 12:26:16 +0000

    All 4 functions now work for parallel

--- ParallelProgramming/parallel/matVec.c
+++ ParallelProgramming/parallel/matVec.c
@@ -3,3 +3,110 @@
 #include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+#define AROW 3
+#define ACOL 5
+
+/* Process mapping function */
+int proc_map(int i, int size)
+{
+    size = size - 1;
+    int r = AROW/size;
+    int proc = i / r;
+    return proc + 1;
+}
+
+int main(int argc, char** argv)
+{
+	srand(0);
+    int size, rank;
+    MPI_Status Stat;
+ 
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+ 
+    if (rank == 0)
+    {
+        int a[AROW][ACOL];
+        int b[ACOL];
+        int c[AROW];
+ 
+        /* Generating Random Values for A & B Array*/
+        for (int i=0;i<AROW;i++)
+        {
+            for (int j=0;j<ACOL;j++)
+            {
+                if (i==0) b[j] = rand() % 1000;
+                a[i][j] = rand() % 1000;
+            }
+        }
+ 
+        /* Printing the Matrix*/
+ 
+        printf("Matrix :\n");
+        for (int i=0;i<AROW;i++)
+        {
+            for (int j=0;j<ACOL;j++)
+            {
+                printf("%3d ", a[i][j]);
+            }
+            printf("\n");
+        }
+        printf("\nVector :\n");
+        for (int i=0;i<ACOL;i++)
+        {
+            printf("%3d \n", b[i]);
+        }
+        printf("\n\n");
+ 
+        /* (1) Sending B Values to other processes */
+        for (int j=1;j<size;j++)
+        {
+            MPI_Send(b, ACOL, MPI_INT, j, 99, MPI_COMM_WORLD);
+        }
+ 
+        /* (2) Sending Required A Values to specific process */
+        for (int i=0;i<AROW;i++)
+        {
+            int processor = proc_map(i, size);
+            MPI_Send(a[i], ACOL, MPI_INT, processor, (100*(i+1)), MPI_COMM_WORLD);
+        }
+ 
+        /* (3) Gathering the result from other processes*/
+        printf("\nOutput: \n");
+        for (int i=0;i<AROW;i++)
+        {
+            int source_process = proc_map(i, size);
+            MPI_Recv(&c[i], 1, MPI_INT, source_process, i, MPI_COMM_WORLD, &Stat);
+            printf("%d\n",c[i]);
+        }
+    }
+    else
+    {
+        int b[ACOL];
+ 
+        /* (1) Each process get B Values from Master */
+        MPI_Recv(b, ACOL, MPI_INT, 0, 99, MPI_COMM_WORLD, &Stat);
+ 
+        /* (2) Get Required A Values from Master then Compute the result */
+        for (int i=0;i<AROW;i++)
+        {
+            int processor = proc_map(i, size);
+            if (rank == processor)
+            {
+                int buffer[ACOL];
+                MPI_Recv(buffer, ACOL, MPI_INT, 0, (100*(i+1)), MPI_COMM_WORLD, &Stat);
+                int sum = 0;
+                for (int j=0;j<ACOL;j++)
+                {
+                    sum = sum + (buffer[j] * b[j] );
+                }
+                MPI_Send(&sum, 1, MPI_INT, 0, i, MPI_COMM_WORLD);
+            }
+        }
+    }
+ 
+    MPI_Finalize();
+    return 0;
+}
