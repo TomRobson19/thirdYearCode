38475a16f6cc9915baacdf2bba07d9e6152dfaf2
unknown <unknown@unknown.com>
2017-03-03 16:46:22 +0000

    Getting matMat setup

--- ParallelProgramming/parallel/matMat.c
+++ ParallelProgramming/parallel/matMat.c
@@ -1,8 +1,9 @@
 //mpicc -g3 matMat.c -o matMat
-//mpirun -n 4 ./matMat
+//mpirun -lm -n 4 ./matMat
 #include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(int argc, char *argv[])
 {
@@ -15,15 +16,15 @@ int main(int argc, char *argv[])
     double totalTime = 0;
 
 	int arow = atoi(argv[1]);
-	int acolumn = atoi(argv[2]);
+	int acolumnbrow = atoi(argv[2]);
 	int bcolumn = atoi(argv[3]);
 
 	double * matrix1;
 	double * matrix2;
 	double * output;
 
-	double * matrixTemporary;
-	double * vectorTemporary;
+	double * matrix1Temporary;
+	double * matrix2Temporary;
 	double * outputTemporary;
 
 	int totalNumberOfProcesses;
@@ -39,52 +40,90 @@ int main(int argc, char *argv[])
 
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	int rowsPerProcess = (int) (ceil( ( (double) row)/( (double) totalNumberOfProcesses)));
+	int rowsPerProcess = (int) (ceil( ( (double) acolumnbrow)/( (double) totalNumberOfProcesses)));
 
 	int totalRows = rowsPerProcess*totalNumberOfProcesses;
 
-	matrix1 = (double *) calloc(arow*acolumn,sizeof(double));
-	matrix2 = (double *) calloc(acolumn*bcolumn,sizeof(double));
+	matrix1 = (double *) calloc(arow*acolumnbrow,sizeof(double));
+	matrix2 = (double *) calloc(acolumnbrow*bcolumn,sizeof(double));
 	output = (double *) calloc(arow*bcolumn,sizeof(double));
 
 	if (rank == 0)
 	{
-		for (int i=0;i<arow;i++)
+		for (int i=0;i<arow*totalRows;i++)
         {
-            for (int j=0;j<acolumn;j++)
-            {
-                a[i][j] = rand() % 1000;
-            }
+            matrix1[i] = rand() % 1000;
         }
-        for (int i=0;i<acolumn;i++)
+        for (int i=0;i<totalRows*bcolumn;i++)
         {
-            for (int j=0;j<bcolumn;j++)
-            {
-                b[i][j] = rand() % 1000;
-            }
+            matrix2[i] = rand() % 1000;
         }
         /* Printing the Matrix*/
         printf("Matrix A :\n");
-        for (int i=0;i<arow;i++)
+        for (int i=0;i<arow*acolumnbrow;i++)
         {
-            for (int j=0;j<acolumn;j++)
+            if(i%acolumnbrow == 0)
             {
-                printf("%3f ", a[i][j]);
+                printf("\n");
             }
-            printf("\n");
+            printf("%3f ", matrix1[i]);
         }
+        printf("\n");
         printf("\nMatrix B :\n");
-        for (int i=0;i<acolumn;i++)
+        for (int i=0;i<acolumnbrow*bcolumn;i++)
         {
-            for (int j=0;j<bcolumn;j++)
+            if(i%bcolumn == 0)
             {
-                printf("%3f ", b[i][j]);
+                printf("\n");
             }
-            printf("\n");
+            printf("%3f ", matrix2[i]);
         }
         printf("\n");
 	}
 
+	matrix1Temporary = (double *) calloc(arow*acolumnbrow,sizeof(double));
+	matrix2Temporary = (double *) calloc(acolumnbrow*bcolumn,sizeof(double));
+	outputTemporary = (double *) calloc(arow*bcolumn,sizeof(double));
+
+	MPI_Bcast(matrix1, arow*totalRows, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+	MPI_Scatter(matrix2, acolumnbrow*rowsPerProcess, MPI_DOUBLE, matrix2Temporary, acolumnbrow*rowsPerProcess, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+	double startTime = MPI_Wtime();
+
+	///MATHS GOES HERE
+
+
+
+
+
+	double endTime = MPI_Wtime();
+
+	//MPI_Gather(outputTemporary, rowsPerProcess, MPI_DOUBLE, output, rowsPerProcess, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+	double temporaryTime = endTime - startTime;
+
+	//MPI_Reduce(&temporaryTime, &totalTime, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+	if(rank == 0)
+	{
+		printf("\nOutput :\n");
+        for (int i=0;i<arow*bcolumn;i++)
+        {
+            if(i%bcolumn == 0)
+            {
+                printf("\n");
+            }
+            printf("%3f ", output[i]);
+        }
+        printf("\n");
+
+		printf("\nTime\n");
+		printf("%f\n", totalTime);
+	}
+
+	MPI_Finalize();
+	return 0;
 }
 
 
