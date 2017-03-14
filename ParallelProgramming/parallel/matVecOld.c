f59994faa9583cbf8c166efcee8051b4bae78292
unknown <unknown@unknown.com>
2017-03-14 10:54:04 +0000

    Brought back old implementations

---
+++ ParallelProgramming/parallel/matVecOld.c
@@ -0,0 +1,138 @@
+//mpicc -g3 matVecOld.c -o matVecOld
+//mpirun -n 4 ./matVecOld
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Process mapping function */
+int mapProcesses(int i, int size, int row)
+{
+    size = size - 1;
+    int r = row/size;
+    int proc = i / r;
+    return proc;
+}
+
+int main(int argc, char** argv)
+{
+	srand(0);
+
+    double totalTime = 0;
+
+	int row = atoi(argv[1]);
+	int column = atoi(argv[2]);
+
+    int size, rank;
+    MPI_Status Stat;
+ 
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+ 
+    if (rank == 0)
+    {
+        double a[row][column];
+        double b[column];
+        double c[row];
+        /* Generating Random Values for A & B Array*/
+        for (int i=0;i<row;i++)
+        {
+            for (int j=0;j<column;j++)
+            {
+                if (i==0) 
+            	{
+            		b[j] = rand() % 1000;
+            	}
+                a[i][j] = rand() % 1000;
+            }
+        }
+        /* Printing the Matrix*/
+        printf("Matrix :\n");
+        for (int i=0;i<row;i++)
+        {
+            for (int j=0;j<column;j++)
+            {
+                printf("%3f ", a[i][j]);
+            }
+            printf("\n");
+        }
+        printf("\nVector :\n");
+        for (int i=0;i<column;i++)
+        {
+            printf("%3f \n", b[i]);
+        }
+        printf("\n");
+ 
+        /* (1) Sending B Values to other processes */
+        for (int j=1;j<size;j++)
+        {
+            MPI_Send(b, column, MPI_DOUBLE, j, 99, MPI_COMM_WORLD);
+        }
+ 
+        /* (2) Sending Required A Values to specific process */
+        for (int i=1;i<row;i++)
+        {
+            int processor = mapProcesses(i, size, row);
+            MPI_Send(a[i], column, MPI_DOUBLE, processor, (100*(i+1)), MPI_COMM_WORLD);
+        }
+
+        double startTime = MPI_Wtime();
+        for (int j=0;j<column;j++)
+        {
+            c[0] = c[0] + (a[0][j] * b[j] );
+        }
+        double endTime = MPI_Wtime();
+
+        double temporaryTime = endTime - startTime;
+        
+        MPI_Reduce(&temporaryTime, &totalTime, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+        /* (3) Gathering the result from other processes*/
+        printf("Output: \n");
+        for (int i=1;i<row;i++)
+        {
+            int source_process = mapProcesses(i, size, row);
+            MPI_Recv(&c[i], 1, MPI_DOUBLE, source_process, i, MPI_COMM_WORLD, &Stat);
+        }
+        for (int i=0;i<row;i++)
+        {
+        	printf("%f\n",c[i]);
+        }
+    }
+    else
+    {
+        double b[column];
+ 
+        /* (1) Each process get B Values from Master */
+        MPI_Recv(b, column, MPI_DOUBLE, 0, 99, MPI_COMM_WORLD, &Stat);
+ 
+        /* (2) Get Required A Values from Master then Compute the result */
+        for (int i=1;i<row;i++)
+        {
+            int processor = mapProcesses(i, size, row);
+            if (rank == processor)
+            {
+                double buffer[column];
+                MPI_Recv(buffer, column, MPI_DOUBLE, 0, (100*(i+1)), MPI_COMM_WORLD, &Stat);
+ 
+                double startTime = MPI_Wtime();
+ 
+                double sum = 0;
+                for (int j=0;j<column;j++)
+                {
+                    sum = sum + (buffer[j] * b[j] );
+                }
+
+                double endTime = MPI_Wtime();
+
+                double temporaryTime = endTime - startTime;
+        
+                MPI_Reduce(&temporaryTime, &totalTime, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+                MPI_Send(&sum, 1, MPI_DOUBLE, 0, i, MPI_COMM_WORLD);
+            }
+        }
+    }
+ 
+    MPI_Finalize();
+    return 0;
+}
---
+++ ParallelProgramming/parallel/vecDotOld.c
@@ -0,0 +1,120 @@
+//mpicc -g3 vecDotOld.c -o vecDotOld
+//mpirun -n 4 ./vecDotOld
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	srand(0);
+    if (argc < 2) {
+        printf("Syntax: test <size of vector>\n");
+        return 1;
+    }
+
+    double totalTime = 0;
+
+    int vectorSize = atoi(argv[1]);
+    int paddedVectorSize = atoi(argv[1]);
+	double * vector1;
+	double * vector2;
+	double * sum;
+
+	int totalNumberOfProcesses;
+
+	int rank;
+
+	int elementsAllocatedPerProcess;
+	
+	MPI_Status status;
+
+	MPI_Init(&argc, &argv);
+
+	MPI_Comm_size(MPI_COMM_WORLD, &totalNumberOfProcesses);
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	
+	double * vector1Temporary;
+	double * vector2Temporary;
+	double * sumTemporary;
+	if (rank == 0)
+	{
+		if (vectorSize%totalNumberOfProcesses != 0)
+		{
+			paddedVectorSize += (totalNumberOfProcesses-(vectorSize%totalNumberOfProcesses));
+		}
+		vector1 = (double *) calloc(paddedVectorSize,sizeof(double));
+		vector2 = (double *) calloc(paddedVectorSize,sizeof(double));
+		sum = (double *) calloc(paddedVectorSize,sizeof(double));
+
+    	MPI_Bcast (&vectorSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    	elementsAllocatedPerProcess = vectorSize/totalNumberOfProcesses;
+
+		for(int i=0;i<vectorSize;i++)
+		{
+			vector1[i] = rand() % 1000;
+			vector2[i] = rand() % 1000;
+		}
+		printf("Vector1: \n");
+		for(int i=0;i<vectorSize;i++)
+		{
+			printf ("%f\n", vector1[i]);
+		}	
+		printf("Vector2: \n");
+		for(int i=0;i<vectorSize;i++)
+		{
+			printf ("%f\n", vector2[i]);
+		}	
+		
+	}
+	MPI_Bcast(&vectorSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&elementsAllocatedPerProcess, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	
+	vector1Temporary = (double *) calloc(elementsAllocatedPerProcess,sizeof(double));
+	vector2Temporary = (double *) calloc(elementsAllocatedPerProcess,sizeof(double));
+	sumTemporary = (double *) calloc(paddedVectorSize,sizeof(double));
+	
+	MPI_Scatter(vector1, elementsAllocatedPerProcess, MPI_DOUBLE, vector1Temporary, elementsAllocatedPerProcess, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	MPI_Scatter(vector2, elementsAllocatedPerProcess, MPI_DOUBLE, vector2Temporary, elementsAllocatedPerProcess, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+	double startTime = MPI_Wtime();
+
+	for(int i=0;i<elementsAllocatedPerProcess;i++)
+	{
+		sumTemporary[i] = vector1Temporary[i]*vector2Temporary[i];
+	}
+
+	MPI_Gather(sumTemporary, elementsAllocatedPerProcess, MPI_DOUBLE, sum, elementsAllocatedPerProcess, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+	double endTime = MPI_Wtime();
+
+	double temporaryTime = endTime - startTime;
+
+	MPI_Reduce(&temporaryTime, &totalTime, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+	
+	if(rank == 0)
+	{
+		double startTime = MPI_Wtime();
+		
+		double dotProduct = 0;
+		for (int i=0;i<vectorSize;i++)
+		{
+			dotProduct += sum[i];
+		}
+
+		double endTime = MPI_Wtime();
+
+		double temporaryTime = endTime - startTime;
+		
+		totalTime += temporaryTime;	
+
+		printf("Dot Product\n");
+		printf("%f\n", dotProduct);
+
+		printf("Time\n");
+		printf("%f\n", totalTime);
+	}
+
+	MPI_Finalize();
+	return 0;
+}
